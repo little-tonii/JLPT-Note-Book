4512135a0e2a61c70f1ac4b0ca620115952f1290
little-chloe <laptrinhcungtony@gmail.com>
2024-09-11 13:53:36 +0700

    new update for application

---
+++ note_book_app/lib/common/colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const k8D493A = Color(0xff8D493A);
+  static const kD0B8A8 = Color(0xffD0B8A8);
+  static const kDFD3C3 = Color(0xffDFD3C3);
+  static const kF8EDE3 = Color(0xffF8EDE3);
+}
