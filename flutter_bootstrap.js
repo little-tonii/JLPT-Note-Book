2bca96754af178d4b2c71e9cb7b4479f00c94de1
little-chloe <little-chloe@users.noreply.github.com>
2024-09-26 03:46:49 +0000

    deploy: 2dcdaf31e6554f5a9f3ca73259e4d5beaae5bf8f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "38300183"
+    serviceWorkerVersion: "3313748260"
   }
 });
